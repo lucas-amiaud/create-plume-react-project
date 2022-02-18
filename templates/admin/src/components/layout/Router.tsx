f7507c6d7b87eee88f2e7122b8aa005e216553d8
Lucas Amiaud <lamiaud@coreoz.com>
2022-02-18 14:03:59 +0100

    add route for log API

--- templates/admin/src/components/layout/Router.tsx
+++ templates/admin/src/components/layout/Router.tsx
@@ -5,19 +5,24 @@ import {
   Redirect,
 } from 'react-router-dom';
 import { getGlobalInstance } from 'plume-ts-di';
+import LogApi from '../../lib/plume-admin-log-api/pages/LogApi';
 import Home from '../pages/Home';
 import Users from '../../lib/plume-admin-users/pages/Users';
 import PermissionRoute from '../theme/routes/PermissionRoute';
 import Permission from '../../services/session/Permission';
-import { HOME, USERS } from '../Routes';
+import { HOME, LOG_API, USERS } from '../Routes';
 
 export default function Router() {
   const users = getGlobalInstance(Users);
+  const logApi = getGlobalInstance(LogApi);
 
   return (
     <Switch>
       <PermissionRoute permission={Permission.MANAGE_USERS} path={USERS}>
         <users.render />
+      </PermissionRoute>
+      <PermissionRoute permission={Permission.MANAGE_API_LOGS} path={LOG_API}>
+        <logApi.render />
       </PermissionRoute>
       <Route path={HOME}>
         <Home />
